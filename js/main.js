d24a8c6bcbec01ce86f336c74d36c07d22505e22
tanveer <mdtanviralamht731@gmail.com>
2022-03-03 16:02:06 +0600

    temperature value added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-const apiKey =`d688b8d4b1e53062089ce9ab2ff36798`;
-const url = `https://api.openweathermap.org/data/2.5/weather?q=dhaka&appid=${apiKey}`;
-const iconLink = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+// const iconLink = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+const searchTemperature = async () => {
+    const apiKey =`d688b8d4b1e53062089ce9ab2ff36798`;
+    const input = document.getElementById('city-name');
+    const inputValue = input.value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}&units=metric`;
+    const response = await fetch(url)
+    const data = await response.json()
+    getTemperature (data);
+    input.value = "";
+}
+const selectID = (id, text)=>{
+    document.getElementById(id).innerText = text;
+}
+const getTemperature = temp =>{
+    selectID('city-name-show', temp.name? temp.name: "No City Found");
+    selectID('Temperature', temp.main.temp? temp.main.temp: ' ');
+    selectID('weather', temp.weather[0].main? temp.weather[0].main: 'No weather');
+    console.log(temp);
+
+}
