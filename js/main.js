263532744c8c99af68e0eaf971523b804415fa3a
tanveer <mdtanviralamht731@gmail.com>
2022-03-03 16:06:22 +0600

    weather icon added

--- js/main.js
+++ js/main.js
@@ -16,6 +16,10 @@ const getTemperature = temp =>{
     selectID('city-name-show', temp.name? temp.name: "No City Found");
     selectID('Temperature', temp.main.temp? temp.main.temp: ' ');
     selectID('weather', temp.weather[0].main? temp.weather[0].main: 'No weather');
+    const image = document.getElementById('weather-icon');
+    const iconUrl = `http://openweathermap.org/img/wn/${temp.weather[0].icon}@2x.png`;
+    console.log(iconUrl);
+    image.setAttribute('src', iconUrl);
     console.log(temp);
 
 }
