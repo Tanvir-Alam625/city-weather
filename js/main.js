5f18b401c97225ba3fc110adae2dfc4e282df6d5
tanveer <mdtanviralamht731@gmail.com>
2022-03-03 15:34:29 +0600

    index added and ready

---
+++ js/main.js
@@ -0,0 +1,3 @@
+const apiKey =`d688b8d4b1e53062089ce9ab2ff36798`;
+const url = `https://api.openweathermap.org/data/2.5/weather?q=dhaka&appid=${apiKey}`;
+const iconLink = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
